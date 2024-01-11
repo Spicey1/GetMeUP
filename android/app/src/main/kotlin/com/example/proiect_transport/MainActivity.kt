4b123ed256aaadb5127fef9f587132cc45983a69
Spicey1 <spiceygk@gmail.com>
2024-01-11 17:58:05 +0200

    Add existing project files to Git

---
+++ android/app/src/main/kotlin/com/example/proiect_transport/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proiect_transport
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/front_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FrontPage extends StatefulWidget {
+  const FrontPage({super.key});
+
+  @override
+  State<FrontPage> createState() => _FrontPageState();
+}
+
+class _FrontPageState extends State<FrontPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Hello'),
+    );
+  }
+}
